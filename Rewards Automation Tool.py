ca8cda9a4bbdef46fa350005f9837af331c1fc3b
123JUICE_BOY321 <110123050+123JUICE-BOY321@users.noreply.github.com>
2024-07-26 22:02:22 +0530

    Add files via upload

---
+++ Rewards Automation Tool.py
@@ -0,0 +1,19 @@
+import pyautogui,random,os
+alphabets="abcdefghijklmnopqrstuvwxyz"
+x=850  #X position of search box.
+y=185  #Y position of search box.
+n=10  #Adjust according to number of searches (points/3).
+sleep=8  #Adjust according to speed of internet and device.
+os.system('start msedge --profile-directory="Profile 1" https://www.bing.com/search?q=bing')
+pyautogui.sleep(sleep)
+pyautogui.moveTo(x, y)
+for i in range(n):
+    pyautogui.click()
+    pyautogui.hotkey('ctrl', 'a')
+    pyautogui.press('backspace')
+    word=random.choice(alphabets)+random.choice(alphabets)+random.choice(alphabets)
+    pyautogui.typewrite(word)
+    pyautogui.press('enter')
+    pyautogui.sleep(sleep)
+os.system('start msedge --profile-directory="Profile 1" https://rewards.bing.com/')
+print("Daily PC search completed.")
