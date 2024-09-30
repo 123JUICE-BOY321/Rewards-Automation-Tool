2c7ea2522735e6313103442f16a204c4c17b6270
123JUICE_BOY321 <110123050+123JUICE-BOY321@users.noreply.github.com>
2024-09-30 23:19:16 +0530

    Update Rewards Automation Tool.py

--- Rewards Automation Tool.py
+++ Rewards Automation Tool.py
@@ -4,7 +4,7 @@ x=850  #X position of search box.
 y=185  #Y position of search box.
 n=10  #Adjust according to number of searches (points/3).
 sleep=8  #Adjust according to speed of internet and device.
-os.system('start msedge --profile-directory="Profile 1" https://www.bing.com/search?q=bing')
+os.system('start msedge --profile-directory="Default" https://www.bing.com/search?q=bing')
 pyautogui.sleep(sleep)
 for i in range(n):
     pyautogui.moveTo(x, y)
@@ -15,5 +15,5 @@ for i in range(n):
     pyautogui.typewrite(word)
     pyautogui.press('enter')
     pyautogui.sleep(sleep)
-os.system('start msedge --profile-directory="Profile 1" https://rewards.bing.com/')
+os.system('start msedge --profile-directory="Default" https://rewards.bing.com/')
 print("Daily PC search completed.")
